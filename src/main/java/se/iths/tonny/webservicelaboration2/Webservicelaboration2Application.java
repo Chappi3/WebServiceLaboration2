1bcacfac5b8c9cfffc58f5c8bf19364a84fd95c0
Tonny <tonny035@hotmail.com>
2019-03-13 18:00:44 +0100

    initial commit

---
+++ src/main/java/se/iths/tonny/webservicelaboration2/Webservicelaboration2Application.java
@@ -0,0 +1,12 @@
+package se.iths.tonny.webservicelaboration2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages ={"se.iths.tonny.webservicelaboration2"})
+public class Webservicelaboration2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Webservicelaboration2Application.class, args);
+    }
+}
