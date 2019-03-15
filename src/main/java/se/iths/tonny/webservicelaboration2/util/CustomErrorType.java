c6f24cf936dbb86ac5cd05e860be0af5f75c08d7
Tonny <tonny035@hotmail.com>
2019-03-15 19:08:56 +0100

    Add CustomErrorType.java

---
+++ src/main/java/se/iths/tonny/webservicelaboration2/util/CustomErrorType.java
@@ -0,0 +1,14 @@
+package se.iths.tonny.webservicelaboration2.util;
+
+public class CustomErrorType {
+
+    private String errorMessage;
+
+    public CustomErrorType(String errorMessage) {
+        this.errorMessage = errorMessage;
+    }
+
+    public String getErrorMessage() {
+        return errorMessage;
+    }
+}
